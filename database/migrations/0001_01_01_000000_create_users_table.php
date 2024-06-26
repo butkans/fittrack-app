a9235d6c37571d0e636eb686788277f2230728b0
butkans <ernestsbutkans11@gmail.com>
2024-06-26 13:23:15 +0300

    otrais push

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id')->constrained()->default(2); // Assuming 2 is the default role for a registered user
             $table->rememberToken();
             $table->timestamps();
         });
